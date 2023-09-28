76715b63a1a3801a3069edf7914aae2e83105977
gao-jinlong <2675130594@qq.com>
2023-09-29 00:01:36 +0800

    feat(IoC):provider of IoC(Inverse of Control)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // @Inject(AppService) // 如果不想在构造函数中注入，可以直接注入到属性中
+  // private readonly appService: AppService;
+  constructor(
+    // 使用 class 作为 token 注入可以不需要 @Inject
+    private readonly appService: AppService,
+    // 使用字符串作为 token 注入需要通过 @Inject
+    @Inject('person') private readonly person: { name: string; age: number },
+    @Inject('github')
+    private readonly github: {
+      name: string;
+      mail: string;
+      appService: AppService;
+      person: {
+        name: string;
+        age: number;
+      };
+    },
+    @Inject('GitHub')
+    private readonly GitHub: {
+      name: string;
+      mail: string;
+      appService: AppService;
+      person: {
+        name: string;
+        age: number;
+      };
+    },
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return `${this.appService.getHello()} 
+     I am ${this.person.name}
+      I am ${this.person.age} years old
+      My github is ${this.GitHub.name}
+     `;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,47 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  // IoC provider 支持的几种注入方式，useClass, useValue, useFactory, useExisting
+  providers: [
+    // AppService, // 这是缩写
+    {
+      // 实际上完整的写法
+      provide: AppService, // 注入 token，可以是字符串或者 symbol
+      useClass: AppService, // 注入的对象的类，Nest 会自动实例化再注入
+    },
+    {
+      // 除了注入 class 之外，还可以注入 value
+      provide: 'person',
+      useValue: {
+        name: 'Ginlon',
+        age: 18,
+      },
+    },
+    {
+      // 支持动态产生值
+      // 支持异步，Nest 会在异步完成之后再注入
+      provide: 'github',
+      // 如果想动态注入，可以使用 useFactory，在参数中注入其他的 token
+      async useFactory(
+        person: { name: string; age: number },
+        appService: AppService,
+      ) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1000);
+        });
+        return {
+          name: 'gaoo-jinlong',
+          mail: '2675130594@qq.com',
+          person,
+          appService,
+        };
+      },
+      inject: ['person', AppService], // 支持注入其他的 token
+    },
+    {
+      provide: 'GitHub', // 别名
+      useExisting: 'github', // 使用已经存在的 token
+    },
+  ],
 })
 export class AppModule {}
